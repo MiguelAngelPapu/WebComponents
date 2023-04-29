a261e752985ab6acea3639ab49ddd814e447f3e9
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2023-04-29 11:20:28 -0500

    feat: :sparkles: Atributos
    Compartir datos entre atributos y sus componentes

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 import mydiv from "./components/my-components.js";
 
-
+// https://lenguajejs.com/javascript/eventos/addeventlistener/
+addEventListener("click", (e)=>{
+    document.querySelector("my-components").dataset.sede = "Bucaramanga";
+})
--- components/my-components.js
+++ components/my-components.js
@@ -1,24 +1,27 @@
-// https://lenguajejs.com/javascript/peticiones-http/url/
 let pathName = new URL(import.meta.url).pathname.replace(".js", ".html")
 let name = pathName.split("/").pop().replace(".html", "")
 
 export default class myDiv extends HTMLElement{
     static async plantilla(){
-        // https://lenguajejs.com/javascript/peticiones-http/fetch/
         return await (await fetch(`${pathName}`)).text();
     }
     constructor(){
         super();        
-        // https://lenguajejs.com/webcomponents/shadow-dom/que-es-shadow-dom/
         let shadowRoot = this.attachShadow({ mode: 'open' });
-
-        // https://lenguajejs.com/javascript/asincronia/promise-api/
         Promise.resolve(myDiv.plantilla())
         .then(html=>{
             shadowRoot.innerHTML = html;
         })
     }
+    // https://lenguajejs.com/webcomponents/componentes/atributos-webcomponent/
+    static get observedAttributes() {
+        return ["data-sede"]
+    }
+    attributeChangedCallback(name, old, now) {
+        document.querySelector("#myH1").innerHTML = now;
+        this.shadowRoot.children.mySede.innerHTML = now;
+        console.log(`El atributo ${name} ha sido modificado de ${old} a ${now}.`);
+    }
     
 }
-// https://lenguajejs.com/webcomponents/funcionalidad/registro-de-custom-elements/
 customElements.define(name, myDiv);
